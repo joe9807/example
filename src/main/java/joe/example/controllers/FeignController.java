c2c03be3602c6fd79cf79cd92cb6f288fbe0697e
joe9807 <joe9807@gmail.com>
2022-12-12 18:42:46 +0300

    refactoring

--- src/main/java/joe/example/controllers/FeignController.java
+++ src/main/java/joe/example/controllers/FeignController.java
@@ -1,6 +1,7 @@
 package joe.example.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import joe.example.client.FeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,7 @@ public class FeignController {
     private FeignClient feignClient;
 
     @Operation(summary = "Send Message from Feign Client")
+    @Parameter(name="number", example="10")
     @GetMapping("/sendNumber")
     public String send(int number) {
         return feignClient.sendNumber(number);
