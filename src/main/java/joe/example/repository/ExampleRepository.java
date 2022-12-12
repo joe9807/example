edbdf8c8003d4d371bec41ce1541878d1b8405a8
joe9807 <joe9807@gmail.com>
2022-12-12 21:34:25 +0300

    refactoring

--- src/main/java/joe/example/repository/ExampleRepository.java
+++ src/main/java/joe/example/repository/ExampleRepository.java
@@ -4,14 +4,6 @@ import joe.example.entity.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ExampleRepository extends JpaRepository<Example, Long> {
-
-    List<Example> findAll();
-
-    Example save(Example example);
-
-    void deleteAll();
 }
